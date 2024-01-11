256684b04b7905e548dd217e77a9d1098563a9f7
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-11 17:46:43 +0100

    update

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Type Checking"""
-from typing import List
+from typing import List, Tuple
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[List[int]]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
@@ -11,7 +11,7 @@ def zoom_array(lst: List[int], factor: int = 2) -> List[List[int]]:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
