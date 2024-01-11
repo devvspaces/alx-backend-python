15e8ed11d36256df44b0e3ac0501ad39e7f6b510
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-11 18:02:17 +0100

    update

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Complex types - list of floats"""
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return the sums of the floats"""
     return sum(input_list)
