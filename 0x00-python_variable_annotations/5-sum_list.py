53c5341e4b015a641b8413af6c65eb40d5405409
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-11 17:44:47 +0100

    update

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Complex types - list of floats"""
+
+
+def sum_list(input_list: float) -> float:
+    """return the sums of the floats"""
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Complex types - mixed list"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """Return sum of list of floats and ints"""
+    return sum(mxd_list)
+
