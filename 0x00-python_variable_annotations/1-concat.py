1ce5f22382d4d8566aca29b5bb5e922f38c0889b
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-11 18:07:56 +0100

    update

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,7 +2,6 @@
 """Basic annotations - Concat"""
 
 
-def concat(str1: str, str2: str) -> str {
+def concat(str1: str, str2: str) -> str:
     """returns a concatenated string"""
     return str1 + str2
-}
