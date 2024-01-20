129e793ed9c805a38efc4cf856ce20cb1d96966c
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-20 05:22:31 +0100

    update

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""The basics of async"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    wait = random.uniform(0, max_delay)
+    await asyncio.sleep(wait)
+    return wait
+
+
+if __name__ == "__main__":
+    print(asyncio.run(wait_random()))
+    print(asyncio.run(wait_random(5)))
+    print(asyncio.run(wait_random(15)))
