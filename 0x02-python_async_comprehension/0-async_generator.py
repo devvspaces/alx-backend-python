4cdbf311c52fc36d217fab28320621305dea02ef
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-20 05:58:17 +0100

    update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     This coroutine loops 10 times, each time asynchronously wait 1 second,
     """
