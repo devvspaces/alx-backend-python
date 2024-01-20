88763fe1493106862298f68c708f0940f4d636a3
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-20 05:37:41 +0100

    update

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""Let's execute multiple coroutines at the same time with async"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def run(response: List[int], max_delay: int):
+    """Run function"""
+    wait = await wait_random(max_delay)
+    response.append(wait)
+
+
+async def wait_n(n: int, max_delay: int = 10) -> float:
+    """Wait for random time"""
+    responses = []
+    tasks = []
+    for _ in range(n):
+        tasks.append(run(responses, max_delay))
+    await asyncio.gather(*tasks)
+    return responses
+
+
+if __name__ == "__main__":
+    print(asyncio.run(wait_n(5, 5)))
+    print(asyncio.run(wait_n(10, 7)))
+    print(asyncio.run(wait_n(10, 0)))
